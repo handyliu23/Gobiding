8f5e66ed26e74f5459fc46b2ad55376a8ce7a966
liucy <liucy@Ctrip.com>
2017-07-07 13:53:00 +0800

    1

---
+++ Mercury/GoBiding/Web/WebForm2.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace GoBiding.Web
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            //txt3.Text = Encoding.UTF8.GetBytes(txt.Text);
+            txt2.Text = Convert.ToBase64String(Encoding.UTF8.GetBytes(txt.Text));
+        }
+    }
+}
